047e892e112987ee8964901a2044b9481745e78b
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-07-22 18:10:10 -0400

    installed npm packages

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,10 +3,10 @@ import { useContext } from 'react'
 import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
-function Gallery(){
+function Gallery() {
     const data = useContext(DataContext)
-    
-    const display = data.map((item,index) => {
+
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
